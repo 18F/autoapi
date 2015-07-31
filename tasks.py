5389c3d2861921c7c84fc58faa676da832a6db03
Joshua Carp <jm.carp@gmail.com>
2015-07-31 18:19:51 -0400

    Clear tables on import.

--- tasks.py
+++ tasks.py
@@ -2,6 +2,7 @@ import os
 import tempfile
 
 import boto
+import sqlalchemy as sa
 from invoke import task, run
 
 EXTENSIONS = [
@@ -9,6 +10,11 @@ EXTENSIONS = [
     '.tsv',
     '.xlsx',
 ]
+
+SQLA_URI = os.getenv(
+    'AUTOAPI_SQLA_URI',
+    ''.join(['sqlite:///', os.path.dirname(__file__), '/autoapi.sqlite']),
+)
 
 def _get_name(path):
     return os.path.splitext(os.path.split(path)[1])[0]
@@ -21,7 +27,7 @@ def requirements(upgrade=True):
     run(cmd)
 
 @task
-def load_bucket(bucket_name, sqla_string, primary_name='id'):
+def load_bucket(bucket_name, primary_name='id'):
     conn = boto.connect_s3()
     bucket = conn.get_bucket(bucket_name)
     keys = bucket.get_all_keys()
@@ -31,27 +37,35 @@ def load_bucket(bucket_name, sqla_string, primary_name='id'):
             continue
         with tempfile.NamedTemporaryFile(suffix=ext) as temp:
             temp.write(key.get_contents_as_string())
-            apify(temp.name, sqla_string, table_name=name, primary_name=primary_name)
+            apify(temp.name, table_name=name, primary_name=primary_name)
 
 @task
-def apify(file_name, sqla_string, table_name=None, primary_name='id', insert=True):
+def apify(file_name, table_name=None, primary_name='id', insert=True):
+    table_name = table_name or _get_name(file_name)
     cmd_csv = 'in2csv {0}'.format(file_name)
     cmd_sql = 'csvsql --db {0} --primary {1} --tables {2}'.format(
-        sqla_string,
+        SQLA_URI,
         primary_name,
-        table_name or _get_name(file_name),
+        table_name,
     )
     if insert:
         cmd_sql += ' --insert'
+    engine = sa.create_engine(SQLA_URI)
+    metadata = sa.MetaData()
+    try:
+        table = sa.Table(table_name, metadata, autoload_with=engine)
+        table.drop(engine)
+    except sa.exc.NoSuchTableError:
+        pass
     cmd = '{0} | {1}'.format(cmd_csv, cmd_sql)
     run(cmd)
 
 @task
-def serve(sqla_string, port=5000, debug=False):
+def serve(port=5000, debug=False):
     from sandman import app
     from sandman.model import activate
     import utils
     app.json_encoder = utils.APIJSONEncoder
-    app.config['SQLALCHEMY_DATABASE_URI'] = sqla_string
+    app.config['SQLALCHEMY_DATABASE_URI'] = SQLA_URI
     activate()
     app.run(port=port, debug=debug)
