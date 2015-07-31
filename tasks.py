7fd615294a58a4534fb426b06ed93380fc529e0d
Joshua Carp <jm.carp@gmail.com>
2015-07-31 17:57:12 -0400

    Support import from S3.

--- tasks.py
+++ tasks.py
@@ -1,5 +1,14 @@
 import os
+import tempfile
+
+import boto
 from invoke import task, run
+
+EXTENSIONS = [
+    '.csv',
+    '.tsv',
+    '.xlsx',
+]
 
 def _get_name(path):
     return os.path.splitext(os.path.split(path)[1])[0]
@@ -12,12 +21,25 @@ def requirements(upgrade=True):
     run(cmd)
 
 @task
-def apify(file_name, sqla_string, primary_name='id', insert=True):
+def load_bucket(bucket_name, sqla_string, primary_name='id'):
+    conn = boto.connect_s3()
+    bucket = conn.get_bucket(bucket_name)
+    keys = bucket.get_all_keys()
+    for key in keys:
+        name, ext = os.path.splitext(key.name)
+        if ext not in EXTENSIONS:
+            continue
+        with tempfile.NamedTemporaryFile(suffix=ext) as temp:
+            temp.write(key.get_contents_as_string())
+            apify(temp.name, sqla_string, table_name=name, primary_name=primary_name)
+
+@task
+def apify(file_name, sqla_string, table_name=None, primary_name='id', insert=True):
     cmd_csv = 'in2csv {0}'.format(file_name)
     cmd_sql = 'csvsql --db {0} --primary {1} --tables {2}'.format(
         sqla_string,
         primary_name,
-        _get_name(file_name),
+        table_name or _get_name(file_name),
     )
     if insert:
         cmd_sql += ' --insert'
