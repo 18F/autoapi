b639b8c70e6aa1cadb4d4c768c0d8a6b02e5953d
Vraj Mohan <radhakrishnan.vrajmohan@gsa.gov>
2016-07-29 11:40:24 -0400

    Verify that float datatype columns are detected

--- test_load_table.py
+++ test_load_table.py
@@ -40,10 +40,10 @@ class TestLoadTable():
         self.meta.reflect()
 
     def test_good_csv(self, tmpdir):
-        CSV = """name, dob, number_of_pets
-        Tom, 1980-02-26, 0
-        Dick, 1982-03-14, 3
-        Harry, 1972-11-24, 2
+        CSV = """name, dob, number_of_pets, weight
+        Tom, 1980-02-26, 0, 146.8
+        Dick, 1982-03-14, 3, 183.1
+        Harry, 1972-11-24, 2, 178.7
         """
 
         self.load_table(tmpdir, CSV, 'people')
@@ -55,6 +55,7 @@ class TestLoadTable():
         assert_column_type(people.columns['name'], str)
         assert_column_type(people.columns['dob'], str)
         assert_column_type(people.columns['number_of_pets'], int)
+        assert_column_type(people.columns['weight'], float)
 
         assert people.columns['id'].primary_key is True
         assert people.columns['name'].primary_key is False
@@ -65,9 +66,9 @@ class TestLoadTable():
         connection = self.engine.connect()
         results = connection.execute(
             select([people]).order_by(people.c.id)).fetchall()
-        assert (1, 'Tom', '1980-02-26', 0) == results[0]
-        assert (2, 'Dick', '1982-03-14', 3) == results[1]
-        assert (3, 'Harry', '1972-11-24', 2) == results[2]
+        assert (1, 'Tom', '1980-02-26', 0, 146.8) == results[0]
+        assert (2, 'Dick', '1982-03-14', 3, 183.1) == results[1]
+        assert (3, 'Harry', '1972-11-24', 2, 178.7) == results[2]
 
     def test_load_with_chunking(self, tmpdir):
         CSV = """name, dob, number_of_pets
